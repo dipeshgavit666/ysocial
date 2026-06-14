35679b825fe007b62b68309b85769b339443eff0
Dipesh Gavit <dipeshgavit333@gmail.com>
2026-06-15 04:41:28 +0530

    completed post routes i guess

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -8,14 +8,20 @@ import {
   updatePost,
 } from "../controllers/post.controllers";
 import { verifyJWT } from "../middlewares/auth.middleware";
+import { createReply, getReplies } from "../controllers/reply.controllers";
+import { toggleLike } from "../controllers/like.controllers";
 
 const router = Router();
 
 router.route("/").post(verifyJWT, createPost);
 router.route("/").get(getAllPosts);
 router.route("/user/:userId").get(getUserPosts);
-router.route("/:id").get(getSinglePost);
-router.route("/:id").delete(verifyJWT, deletePost);
-router.route("/:id").put(verifyJWT, updatePost);
+router.route("/:postId").get(getSinglePost);
+router.route("/:postId").delete(verifyJWT, deletePost);
+router.route("/:postId").put(verifyJWT, updatePost);
+
+router.route("/:postId").post(verifyJWT, createReply);
+router.route("/:postId/replies").get(getReplies);
+router.route("/:postId/like").patch(verifyJWT, toggleLike);
 
 export default router;

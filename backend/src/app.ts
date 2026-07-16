0e4a465d45d109d33056b0d9c7a542055379eb9f
Dipesh <dipeshgavit333@gmail.com>
2026-07-17 02:14:07 +0530

    chore: add Docker Compose configuration and create Layout and Navbar components

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") || "http://locahost:5173",
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Conent-Type", "Authorization"],

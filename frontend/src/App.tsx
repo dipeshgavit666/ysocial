156f88351ed21ee8ed922a34247353573f85c0b0
Dipesh <dipeshgavit333@gmail.com>
2026-07-21 11:56:32 +0530

    feat: implement ProtectedRoute component for user authentication enforcement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { Layout } from "./components/Layout";
 import { HomeFeed } from "./pages/HomeFeed";
 import { ProfilePage } from "./pages/ProfilePage";
 import { AuthPage } from "./pages/AuthPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<HomeFeed />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile/:username" element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Route>
       </Route>
       <Route path="/auth" element={<AuthPage />} />
     </Routes>
